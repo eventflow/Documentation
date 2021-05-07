43d3b14c997009f48ef6d309a34a8c4c27fd32ca
Rasmus Mikkelsen <r@smus.nu>
2021-05-07 12:07:18 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -41,7 +41,7 @@ http://cakebuild.net
 [CmdletBinding()]
 Param(
     [string]$Script = "build.cake",
-    [string]$Target = "Package",
+    [string]$Target = "All",
     [ValidateSet("Release", "Debug")]
     [string]$Configuration = "Release",
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
